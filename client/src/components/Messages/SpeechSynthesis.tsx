62e81eb7f411796f0d7b438778e3118be7be4d21
bsu3338 <bsu3338@users.noreply.github.com>
2023-08-09 00:16:15 -0500

    Update SpeechSynthesis.tsx

--- client/src/components/Messages/SpeechSynthesis.tsx
+++ client/src/components/Messages/SpeechSynthesis.tsx
@@ -1,42 +1,28 @@
 import { useState, useEffect } from 'react';
 
 function useSpeechSynthesis() {
-  const [isEnabled, setIsEnabled] = useState(false);
+  const [isSpeechEnabled, setIsSpeechEnabled] = useState(false);
   const [textToSpeak, setTextToSpeak] = useState('');
 
   const synthesizeSpeech = (text) => {
-    setTextToSpeak(text);
+    if (isSpeechEnabled) {
+      setTextToSpeak(text);
+    }
+  };
+
+  const toggleSpeechSynthesis = () => {
+    setIsSpeechEnabled(!isSpeechEnabled);
   };
 
   useEffect(() => {
-    const speak = () => {
-      if (!isEnabled) return; // Don't speak if isEnabled is false
-      const synth = window.speechSynthesis;
-      const utterance = new SpeechSynthesisUtterance(textToSpeak);
-      synth.speak(utterance);
-    };
+    if (!isSpeechEnabled || !textToSpeak) return;
 
-    const handleKeyDown = (event) => {
-      if (event.shiftKey && event.altKey && event.key === 'P') {
-        console.log('Speech Toggle', isEnabled);
-        setIsEnabled((prevEnabled) => {
-          const newState = !prevEnabled;
-          if (newState) {
-            speak(); // Speak if isEnabled is being set to true
-          }
-          return newState;
-        });
-      }
-    };
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(textToSpeak);
+    synth.speak(utterance);
+  }, [textToSpeak, isSpeechEnabled]);
 
-    window.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [textToSpeak, isEnabled]);
-
-  return { synthesizeSpeech };
+  return { synthesizeSpeech, toggleSpeechSynthesis, isSpeechEnabled };
 }
 
 export default useSpeechSynthesis;
