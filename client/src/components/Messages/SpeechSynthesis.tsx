9af637e0f55716cdd975bd91359a3d6899ccacd3
bsu3338 <bsu3338@users.noreply.github.com>
2023-08-08 23:41:17 -0500

    Update SpeechSynthesis.tsx

--- client/src/components/Messages/SpeechSynthesis.tsx
+++ client/src/components/Messages/SpeechSynthesis.tsx
@@ -1,34 +1,37 @@
 import { useState, useEffect } from 'react';
 
-function useSpeechSynthesis(text) {
-  const [isEnabled, setIsEnabled] = useState(false);
-  console.log('SpeechSynth',text);
-  useEffect(() => {
-    const speak = () => {
-      const synth = window.speechSynthesis;
-      const utterance = new SpeechSynthesisUtterance(text);
-      synth.speak(utterance);
-    };
+function useSpeechSynthesis() {
+  const synthesizeSpeech = (text) => {
+    const [isEnabled, setIsEnabled] = useState(false);
+    console.log('SpeechSynth',text);
+    useEffect(() => {
+      const speak = () => {
+        const synth = window.speechSynthesis;
+        const utterance = new SpeechSynthesisUtterance(text);
+        synth.speak(utterance);
+      };
 
-    const handleKeyDown = (event) => {
-      if (event.shiftKey && event.altKey && event.key === 'P') {
-        console.log('Speech Toggle');
-        setIsEnabled((prevEnabled) => {
-          const newState = !prevEnabled;
-          if (!newState) {
-            speak();
-          }
-          return newState;
-        });
-      }
-    };
+      const handleKeyDown = (event) => {
+        if (event.shiftKey && event.altKey && event.key === 'P') {
+          console.log('Speech Toggle');
+          setIsEnabled((prevEnabled) => {
+            const newState = !prevEnabled;
+            if (!newState) {
+              speak();
+            }
+            return newState;
+          });
+        }
+      };
 
-    window.addEventListener('keydown', handleKeyDown);
+      window.addEventListener('keydown', handleKeyDown);
     
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [text, isEnabled]);
+      return () => {
+        window.removeEventListener('keydown', handleKeyDown);
+      };
+    }, [text, isEnabled]);
+  }
+  return { synthesizeSpeech };
 }
-
+  
 export default useSpeechSynthesis;
