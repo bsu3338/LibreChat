627a1bec774653d4cb81620a0c91504a55295956
bsu3338 <bsu3338@users.noreply.github.com>
2023-08-09 11:10:32 -0500

    Update KeyboardShortcuts.tsx

--- client/src/components/Input/KeyboardShortcuts.tsx
+++ client/src/components/Input/KeyboardShortcuts.tsx
@@ -1,1 +1,33 @@
 
+import useSpeechRecognition from './SpeechRecognition';
+import useSpeechSynthesis from '../Messages/SpeechSynthesis';
+
+const handleGlobalKeyDown = (event) => {
+  const { toggleListening } = useSpeechRecognition();
+  const { toggleSpeechSynthesis } = useSpeechSynthesis();
+
+  
+  if (event.shiftKey && event.altKey) {
+    if (event.key === 'L') {
+      // Logic related to speech recognition
+      toggleSpeechRecognition();
+    }
+    if (event.key === 'P') {
+      // Logic related to speech synthesis
+      toggleSpeechSynthesis();
+    }
+    if (event.key === 'O') {
+      // Other logic as needed
+      //toggleAutoListen();
+    }
+  }
+};
+
+// Inside a component or higher-order function:
+useEffect(() => {
+  window.addEventListener('keydown', handleGlobalKeyDown);
+
+  return () => {
+    window.removeEventListener('keydown', handleGlobalKeyDown);
+  };
+}, []);
