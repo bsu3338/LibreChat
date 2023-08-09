c34692f6ee6a60552675c89a856a3bbdcaa5f79f
bsu3338 <bsu3338@users.noreply.github.com>
2023-08-08 21:08:10 -0500

    Create SpeechSynthesis.tsx

---
+++ client/src/components/Messages/SpeechSynthesis.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+function useSpeechSynthesis(text) {
+  useEffect(() => {
+    const speak = () => {
+      const synth = window.speechSynthesis;
+      const utterance = new SpeechSynthesisUtterance(text);
+      synth.speak(utterance);
+    };
+
+    const handleKeyDown = (event) => {
+      if (event.shiftKey && event.altKey && event.code === 'KeyP') {
+        speak();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [text]);
+}
