e88d343c015547d7c82c7edd5391a8d48801beba
bsu3338 <bsu3338@users.noreply.github.com>
2024-03-31 18:38:30 -0500

    Update Tools.tsx

--- client/src/components/Nav/SettingsTabs/Tools/Tools.tsx
+++ client/src/components/Nav/SettingsTabs/Tools/Tools.tsx
@@ -4,7 +4,7 @@ import * as Tabs from '@radix-ui/react-tabs';
 import { SettingsTabValues } from 'librechat-data-provider';
 import { Switch } from '~/components/ui';
 import { useLocalize } from '~/hooks';
-import Avatar from './Avatar';
+import StableDiffusion from './StableDiffusion';
 import store from '~/store';
 
 function Tools({ onCheckedChange }: { onCheckedChange?: (value: boolean) => void }) {
@@ -26,17 +26,7 @@ function Tools({ onCheckedChange }: { onCheckedChange?: (value: boolean) => void
     >
       <div className="flex flex-col gap-3 text-sm text-gray-600 dark:text-gray-50">
         <div className="border-b pb-3 last-of-type:border-b-0 dark:border-gray-700">
-          <Avatar />
-        </div>
-        <div className="flex items-center justify-between">
-          <div> {localize('com_nav_user_name_display')} </div>
-          <Switch
-            id="UsernameDisplay"
-            checked={UsernameDisplay}
-            onCheckedChange={handleCheckedChange}
-            className="ml-4 mt-2"
-            data-testid="UsernameDisplay"
-          />
+          <StableDiffusion />
         </div>
       </div>
       <div className="border-b pb-3 last-of-type:border-b-0 dark:border-gray-700"></div>
