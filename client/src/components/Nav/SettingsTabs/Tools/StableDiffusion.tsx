2a2a3cb9e1ebfc4c4135fb674fb35193c0e26e19
bsu3338 <bsu3338@users.noreply.github.com>
2024-04-02 18:18:25 -0500

    Update StableDiffusion.tsx

--- client/src/components/Nav/SettingsTabs/Tools/StableDiffusion.tsx
+++ client/src/components/Nav/SettingsTabs/Tools/StableDiffusion.tsx
@@ -1,6 +1,7 @@
 import { useRecoilState } from 'recoil';
 import * as Tabs from '@radix-ui/react-tabs';
 import React, { useCallback, useRef } from 'react';
+import { useGetStartupConfig } from 'librechat-data-provider/react-query';
 import {
   useLocalize,
   useLocalStorage,
@@ -9,53 +10,59 @@ import { Dropdown } from '~/components/ui';
 import store from '~/store';
 
 export const SDProfileSelector = ({
-  langcode,
+  sdprofile,
   onChange,
 }: {
-  langcode: string;
+  sdprofile: string;
   onChange: (value: string) => void;
 }) => {
   const localize = useLocalize();
 
   // Create an array of options for the Dropdown
-  const languageOptions = [
-    { value: 'auto', display: 'SDXL 1.5' },
-    { value: 'en-US', display: 'SDXL Turbo' },
-  ];
+  const { data: config } = useGetStartupConfig();
+  const sdConfig = config?.tools?.stableDiffusion;
+
+  let sdProfileOptions = [];
+  if (sdConfig) {
+    sdProfileOptions = sdConfig.map(obj => {
+      return {
+        value: obj.name,
+        display: obj.name
+      };
+    });
+  } else {
+    sdProfileOptions = [
+      { value: 'Default', display: localize('com_nav_sd_default') },
+    ];
+  }
 
   return (
     <div className="flex items-center justify-between">
-      <div> Stable Diffusion Profile </div>
-      <Dropdown value={langcode} onChange={onChange} options={languageOptions} />
+      <div> {localize('com_nav_sd_profile')} </div>
+      <Dropdown value={sdprofile} onChange={onChange} options={sdProfileOptions} />
     </div>
   );
 };
 
 export default function StableDiffusion() {
-  const [langcode, setLangcode] = useRecoilState(store.lang);
-  const [selectedLang, setSelectedLang] = useLocalStorage('selectedLang', langcode);
+  const [sdprofile, setSDProfile] = useRecoilState(store.sdProfile);
+  const [selectedSDProfile, setSelectedSDProfile] = useLocalStorage('selectedSDProfile', sdprofile);
 
   const contentRef = useRef(null);
 
-  const changeLang = useCallback(
+  const changeSDProfile = useCallback(
     (value: string) => {
-      setSelectedLang(value);
-      if (value === 'auto') {
-        const userLang = navigator.language || navigator.languages[0];
-        setLangcode(userLang);
-        localStorage.setItem('lang', userLang);
-      } else {
-        setLangcode(value);
-        localStorage.setItem('lang', value);
-      }
+      setSelectedSDProfile(value);
+      setSDProfile(value);
+      localStorage.setItem('sdProfile', value);
     },
-    [setLangcode, setSelectedLang],
+    [setSDProfile, setSelectedSDProfile],
   );
 
   return (
       <div className="flex flex-col gap-3 text-sm text-gray-600 dark:text-gray-50">
         <div className="border-b pb-3 last-of-type:border-b-0 dark:border-gray-700">
-          <SDProfileSelector langcode={selectedLang} onChange={changeLang} />
+          <SDProfileSelector sdprofile={selectedSDProfile} onChange={changeSDProfile} />
         </div>
       </div>
   );
