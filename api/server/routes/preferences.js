f450352eeac74a1450c985796aff65746a723c9f
bsu3338 <bsu3338@users.noreply.github.com>
2024-04-09 17:09:55 -0500

    Update and rename toolsconfig.js to preferences.js

--- api/server/routes/preferences.js
+++ api/server/routes/preferences.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { updateToolsConfig, getToolsConfig } = require('../services/UserService');
+const { updatePreference, getPreference } = require('../services/UserService');
 const { requireJwtAuth } = require('../middleware/');
 
 router.put('/', requireJwtAuth, async (req, res) => {
-  await updateToolsConfig({ userId: req.user.id, ...req.body });
+  await updatePreference({ userId: req.user.id, ...req.body });
   res.status(201).send();
 });
 
 router.get('/', requireJwtAuth, async (req, res) => {
   const { name } = req.query;
-  const response = await getToolsConfig({ userId: req.user.id, name });
+  const response = await getPreference({ userId: req.user.id, name });
   res.status(200).send(response);
 });
 
